d617805c36ed3ea62adebef141171878f014ed39
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-19 13:31:44 +0300

    express app configurations

--- config/constants.js
+++ config/constants.js
@@ -1,1 +1,3 @@
 exports.CONNECTION_STRING = "mongodb://127.0.0.1:27017/Social-Network";
+exports.SECRET = "kfj348gnorigenirgignwgn8wnwgnrenigge8gnrn";
+exports.SALT_ROUNDS = 15;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const port = 3030;
 const cors = require('cors');
 const { initDatabase } = require('../config/database');
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 initDatabase()
     .then(() => {
         console.log("Database connected successfully!");
