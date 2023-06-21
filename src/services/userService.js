584a0d7d55710998c2d5840f341e8c83222e65bc
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-21 07:53:25 +0300

    register user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,16 @@
 const User = require('../models/User');
+const { SALT_ROUNDS } = require('../../config/constants');
+const bcrypt = require('bcrypt');
+
+exports.registerUser = async (userData) => {
+    const user = await User.findOne({ email: userData.email });
+
+    if (user) {
+        return "User with this email already exists!";
+    } else {
+        const salt = await bcrypt.genSalt(SALT_ROUNDS);
+        const password = await bcrypt.hash(userData.password, salt);
+        const user = await User.create({ ...userData, password: password });
+        return user;
+    }
+};
