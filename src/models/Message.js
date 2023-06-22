b9be098d7981b1510759b1a64a43216ab117d49d
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-22 19:31:56 +0300

    apply timestamps to the model

--- src/models/Message.js
+++ src/models/Message.js
@@ -8,8 +8,12 @@ const messageSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String,
+        required: true
     }
-});
+}, { timestamps: true });
 
 const Message = mongoose.model('Message', messageSchema);
 module.exports = Message;
