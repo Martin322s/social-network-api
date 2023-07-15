94352cbf0a66fb2077c1cf17e267d74b80a43092
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-15 16:25:12 +0300

    database object data

--- src/controllers/publicationController.js
+++ src/controllers/publicationController.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 
 router.post('/create', (req, res) => {
-    const { opinion, file } = req.body;
-    
+    const { opinion, file, userId } = req.body;
+    const databaseData = {
+        content: opinion,
+        image: file,
+        likes: 0,
+        dislikes: 0,
+        author: userId,
+        comments: []
+    };
+    console.log(databaseData);
 });
 
 module.exports = router;
